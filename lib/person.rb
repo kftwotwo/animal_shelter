c4180b1cd486594e134b5694d69d806d796a30ac
Kenneth Black and Kevin Finley <kenneth@>
2016-12-06 10:02:41 -0800

    add type and breed pref

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,11 @@
 class Person
-  attr_reader(:name, :phone)
+  attr_reader(:name, :phone, :type_pref, :breed_pref)
 
-  def initialize(attributes)
-    @name = attributes[:name]
-    @phone = attributes[:phone]
+  def initialize(attrs)
+    @name = attrs[:name]
+    @phone = attrs[:phone]
+    @type_pref = attrs[:type_pref => nil]
+    @breed_pref = attrs[:breed_pref => nil]
   end
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,8 +4,10 @@ describe(Person) do
   describe('#initialize') do
     it "will return name" do
       person = Person.new({:name => "Kenny", :phone => '406-555-1234'})
-      expect(person.name()).to(eq("Kenny"))
-      expect(person.phone()).to(eq('406-555-1234'))
+      expect(person.name).to eq "Kenny"
+      expect(person.phone).to eq '406-555-1234'
+      expect(person.type_pref).to eq nil
+      expect(person.breed_pref).to eq nil
     end
   end
 

4f0710292a2c2fe7ab7230285b55b429314ed329
Kenneth Black and Kevin Finley <kevin@>
2016-12-06 10:18:20 -0800

    add #id and pass fix defaults for type and breed

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 class Person
-  attr_reader(:name, :phone, :type_pref, :breed_pref)
+  attr_reader(:id, :name, :phone, :type_pref, :breed_pref)
 
   def initialize(attrs)
+    @id = attrs[:id => nil]
     @name = attrs[:name]
     @phone = attrs[:phone]
-    @type_pref = attrs[:type_pref => nil]
-    @breed_pref = attrs[:breed_pref => nil]
+    @type_pref = attrs[:type_pref]
+    @breed_pref = attrs[:breed_pref]
   end
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,13 +1,30 @@
 require('spec_helper')
 
-describe(Person) do
-  describe('#initialize') do
-    it "will return name" do
-      person = Person.new({:name => "Kenny", :phone => '406-555-1234'})
-      expect(person.name).to eq "Kenny"
-      expect(person.phone).to eq '406-555-1234'
-      expect(person.type_pref).to eq nil
-      expect(person.breed_pref).to eq nil
+describe Person  do
+  before :each  do
+    @person = Person.new({:name => "Kenny", :phone => '406-555-1234'})
+    @person2 = Person.new({
+      :name => "Kevin",
+      :phone => '406-555-5678',
+      :type_pref => 'dog',
+      :breed_pref => 'labs'
+    })
+  end
+  describe '#initialize'  do
+    it "will return name and phone" do
+      expect(@person.name).to eq "Kenny"
+      expect(@person.phone).to eq '406-555-1234'
+    end
+
+    it "will initialize id, type and breed preferences as nil if not supplied" do
+      expect(@person.id).to eq nil
+      expect(@person.type_pref).to eq nil
+      expect(@person.breed_pref).to eq nil
+    end
+
+    it "will initialize breed preferences properly if provided" do
+      expect(@person2.type_pref).to eq "dog"
+      expect(@person2.breed_pref).to eq "labs"
     end
   end
 
